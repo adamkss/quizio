afc4c6fff6047916fc1acfc9d88dd434f39498cd
Adam Kiss <kiss.adam.a@gmail.com>
2019-10-23 23:02:24 +0200

    update API

--- utils/QuizRequests.js
+++ utils/QuizRequests.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = 'https://ec2-18-221-120-157.us-east-2.compute.amazonaws.com';
+const baseURL = 'http://ec2-18-221-120-157.us-east-2.compute.amazonaws.com:4000';
 // const baseURL = 'https://localhost';
 
 axios.defaults.baseURL = baseURL;
