505aaa0bca858a4ee6e3d3171b591ec71afe90f2
Adam Kiss <kiss.adam.a@gmail.com>
2019-11-08 15:55:25 +0200

    asd

--- utils/QuizRequests.js
+++ utils/QuizRequests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const baseURL = 'http://ec2-3-133-200-212.us-east-2.compute.amazonaws.com:4000';
-const baseURL = 'http://localhost:4000';
+const baseURL = 'http://ec2-3-133-200-212.us-east-2.compute.amazonaws.com:4000';
+// const baseURL = 'http://localhost:4000';
 
 axios.defaults.baseURL = baseURL;
 
