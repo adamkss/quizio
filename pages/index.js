1ad32dab5fa04189813fdbe51a7a3b6d32136156
Adam Kiss <kiss.adam.a@gmail.com>
2019-11-08 15:48:29 +0200

    -change index page -add more content to quizzio main page

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,269 @@
-import { useEffect, useRef } from 'react';
-import { TweenMax } from 'gsap';
-import Link from 'next/link';
-import LayoutSetup from '../components/layoutSetup';
+import LayoutSetup from "../components/layoutSetup"
+import React, { useRef, useEffect, useState } from 'react';
+import Router from 'next/router';
 
-const FeatureCard = ({ title, picturePath }) => {
+export default () => {
+    const onClickCreateQuiz = React.useCallback(() => {
+        Router.push('/createGenericQuiz');
+    }, []);
+
+    const mainContentRef = useRef(null);
+    const [isHeaderSticky, setIsHeaderSticky] = useState(false);
+
+    useEffect(() => {
+        const scrollEventListener = () => {
+            const scrollTop = mainContentRef.current.scrollTop;
+            if (scrollTop >= 50) {
+                setIsHeaderSticky(true);
+            } else {
+                setIsHeaderSticky(false);
+            }
+        };
+
+        mainContentRef.current.addEventListener('scroll', scrollEventListener);
+        return () => {
+            mainContentRef.current.removeEventListener('scroll', scrollEventListener);
+        }
+    }, []);
+
     return (
         <>
-            <div className="feature-card">
-                <h1>{title}</h1>
-                <img src={picturePath} />
+            <LayoutSetup />
+            <header className={`${isHeaderSticky ? "sticky" : ""}`}>
+                <h1>Quizzio</h1>
+                <nav>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>The flow</a></li>
+                        <li><a>About us</a></li>
+                        <li><a>Login</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main ref={mainContentRef}>
+                <section className="entry-stuff-container">
+                    <h1>Quizzio</h1>
+                    <p className="subtitle">Design the most beautiful quizzes</p>
+                    <button className="primary-button" onClick={onClickCreateQuiz}>
+                        Create quiz now
+                    </button>
+                    <p>...or</p>
+                    <button className="secondary-button">
+                        Login
+                    </button>
+                </section>
+                <section className="quiz-illustration-container">
+                    <QuizzIllustration />
+                </section>
+            </main>
+            <style jsx>
+                {`
+                header {
+                    display: flex;
+                    height: 50px;
+                    transition: all 0.3s;
+                    align-items: center;
+                }
+                header h1 {
+                    flex-grow: 1;
+                    padding-left: 10px;
+                    transition: all 0.3s;
+                    opacity: 0;
+                    font-size: 1.4em;
+                }
+                header.sticky h1 {
+                    opacity: 1;
+                }
+                header.sticky {
+                    box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.4);
+                }
+                nav ul {
+                    display: flex;
+                }
+                nav li {
+                    padding: 2px;
+                    list-style-type: none;
+                    font-size: 1em;
+                    margin-right: 5px;
+                }
+                @media(min-width: 400px) {
+                    nav li {
+                        font-size: 1.2em;
+                        margin-right: 10px;
+                    }
+                }
+                main {
+                    width: 100%;
+                    height: calc(100vh - 50px);
+                    position: relative;
+                    overflow-y: auto;
+                }
+                main button{
+                    font-family: inherit;
+                    border: 0;
+                    outline: none;
+                    width: 200px;
+                    border-radius: 12px;
+                    color: white;
+                    font-size: 1.15em;
+                    padding: 9px;
+                    cursor: pointer;
+                    transition: all 0.3s;
+                }
+                .primary-button {
+                    box-shadow: 1px 1px 5px blue;
+                    background-color: blue;
+                    margin-top: 50px;
+                }
+                .primary-button:hover,
+                .primary-button:active {
+                    background-color: #0040FF;
+                    box-shadow: 1px 1px 5px #0040FF;
+                }
+                .secondary-button {
+                    margin-top: 10px;
+                    background-color: #F64D72;
+                    box-shadow: 1px 1px 5px #F64D72;
+                }
+                .secondary-button:hover,
+                .secondary-button:active {
+                    background-color: #FF4D90;
+                    box-shadow: 1px 1px 5px #FF4D90;
+                }
+                main p {
+                    font-size: 1.5em;
+                    margin-top: 10px;
+                }
+                
+                .quiz-illustration-container {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    transform: rotate(3deg);
+                    margin-top: 50px;
+                    animation: QuizIllustrationIntro 0.7s ease-out;
+                }
+                @media (min-width: 1195px) {
+                    .quiz-illustration-container {
+                        position: fixed;
+                        top: 0px;
+                        bottom: 0px;
+                        left: -5px;
+                    }
+                }
+                @keyframes QuizIllustrationIntro { 
+                    0% {
+                        transform: rotate(0deg);
+                        left: -30px;
+                        opacity: 0;
+                    }
+                    100% {
+                        transform: rotate(3deg);
+                        left: -5px;
+                        opacity: 1;
+                    }
+                }
+                .entry-stuff-container {
+                    width: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    animation: SlideDownAndFadeIn 1s;
+                }
+                .entry-stuff-container h1 {
+                    font-size: 7em;
+                    font-weight: 700;
+                }
+                .entry-stuff-container p.subtitle  {
+                    font-size: 3em;
+                    text-align: center;
+                }
+                @keyframes SlideDownAndFadeIn {
+                    0% {
+                        transform: translateY(-20px);
+                        opacity: 0;
+                    }
+                    100% {
+                        transform: translateY(0px);
+                        opacity: 1;
+                    }
+                }
+            `}
+            </style>
+        </>
+    )
+}
+
+const QuizzIllustration = () => {
+    return (
+        <>
+            <div className="shell">
+                <h1>Hard Math Question</h1>
+                <SillyHandwritingWithOption text="Between 1914 - 1918" />
+                <SillyHandwritingWithOption text="sin(45)" checked />
+                <SillyHandwritingWithOption text="Mozart" />
             </div>
             <style jsx>
                 {`
-                    .feature-card {
-                        width: 185px;
-                        height: 185px;
-                        background-color: white;
-                        box-shadow: 0px 0px 10px grey;
-                        border-radius: 10px;
-                        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-                        transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-                        position: relative;
+                    .shell {
+                        width: 300px;
+                        box-shadow: 2px 2px 13px grey;
+                        border-radius: 5px;
+                        padding: 30px;
+                        padding-top: 20px;
+                        padding-bottom: 100px;
                     }
-
-                    .feature-card h1 {
+                    @media (min-width: 500px) {
+                        .shell {
+                            width: 350px;
+                        }
+                    }
+                    @media (min-width: 1400px) {
+                        .shell {
+                            width: 400px;
+                        }
+                    }
+                    h1 {
                         text-align: center;
                         font-weight: 400;
-                        font-size: 1.6em;
+                        font-size: 2em;
                     }
+            `}
+            </style>
+        </>
+    )
+}
 
-                    .feature-card img {
-                        position: absolute;
-                        width: 80px;
-                        top: 45%;
-                        left: 50%;
-                        transform: translatex(-50%);
+const SillyHandwritingWithOption = ({ text, checked }) => {
+    return (
+        <>
+            <div className="shell">
+                <SillyHandwriting text={text} />
+                <Checkmarkplace checked={checked} />
+            </div>
+            <style jsx>
+                {`
+                    .shell {
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center;
+                        margin-top: 60px;
                     }
-
-                    .feature-card:hover {
-                        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-                    }
-
-                    .feature-card:not(:nth-child(1)) {
-                        margin-left: 25px;
+            `}
+            </style>
+        </>
+    )
+}
+const SillyHandwriting = ({ text = "Lorem impsum dolor" }) => {
+    return (
+        <>
+            <span>{text}</span>
+            <style jsx>
+                {`
+                    span {
+                        font-size: 2em;
+                        font-family: 'Reenie Beanie', cursive;
                     }
                 `}
             </style>
@@ -50,216 +271,30 @@ const FeatureCard = ({ title, picturePath }) => {
     )
 }
 
-export default () => {
-    let headingElement = useRef(null);
-    let subheadingElement = useRef(null);
-    let headingImageElement = useRef(null);
-
-    let featureCardsHolder = useRef(null);
-
-    useEffect(() => {
-        TweenMax.from(
-            headingElement,
-            1,
-            {
-                y: 40,
-                opacity: 0
-            },
-        );
-        TweenMax.from(
-            subheadingElement,
-            1,
-            {
-                y: 40,
-                opacity: 0,
-                delay: 0.3
-            },
-        );
-        TweenMax.from(
-            headingImageElement,
-            1,
-            {
-                x: 40,
-                opacity: 0,
-                delay: 0.3
-            }
-        );
-        TweenMax.from(
-            featureCardsHolder,
-            1,
-            {
-                y: 40,
-                opacity: 0,
-                delay: .3
-            }
-        )
-    }, []);
-
+const Checkmarkplace = ({ checkmarkplaceColor = "#44318d", checked }) => {
     return (
         <>
-            <LayoutSetup/>
-            <header>
-                <div className="horizontal-centered justify-space-around">
-                    <section>
-                        <h1 ref={ref => headingElement = ref}>Conquerio</h1>
-                        <h2 ref={ref => subheadingElement = ref}>Începe-ti cariera în IT <span className="bold">acum.</span></h2>
-                    </section>
-                    <img src="/static/macbook.png" ref={ref => headingImageElement = ref} />
-                </div>
-                <section className="horizontal-centered feature-cards-holder" ref={ref => featureCardsHolder = ref}>
-                    <FeatureCard title="Învață și câștigă puncte" picturePath="/static/trophy.svg" />
-                    <FeatureCard title="Urmărește-ți progresul" picturePath="/static/king.svg" />
-                    <FeatureCard title="Primește diplome" picturePath="/static/diploma.svg" />
-                </section>
-
-                <button className="see-details">Să începem! Înregistrare.</button>
-                <Link href="/login">
-                    <button className="login-already-user">Deja utilizator? Autentificare.</button>
-                </Link>
-            </header>
-
-            <main>
-                <section className="feature-detail first-detail">
-
-                </section>
-                <section className="feature-detail second-detail">
-                </section>
-            </main>
-
-            <style jsx global>
-                {`
-                body, html {
-                    padding: 0;
-                    margin: 0;
-                    font-family: 'Oswald', sans-serif;
+            <div className="checkmark-place">
+                {checked ?
+                    <img src="/static/checkmark.svg" />
+                    :
+                    null
                 }
-                h1, h2 {
-                    margin: 0;
-                }
-                `}
-            </style>
+            </div>
             <style jsx>
                 {`
-
-            header {
-                width: 100%;
-                padding-bottom: 40px;
-                background: rgb(0,27,103);
-                background: linear-gradient(270deg, rgba(0,27,103,1) 0%, rgba(78,103,235,1) 100%);
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                justify-content: space-around;
-                clip-path: polygon(50% 0%, 100% 0, 100% 93%, 50% 100%, 0 93%, 0 0);
-            }
-            .horizontal-centered {
-                width: 100%;
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: center;
-            }
-            .justify-space-around {
-                justify-content: space-around;
-            }
-
-            button.see-details {
-                outline: none;
-                height: 40px;
-                padding-left: 10px;
-                padding-right: 10px;
-                border-radius: 10px;
-                background: linear-gradient(90deg, #EF34F5 0%, #E74C54 100%);
-                border: none;
-                color: white;
-                font-size: 1.4em;
-                font-family: 'Oswald', sans-serif;
-                box-shadow: 0px 0px 9px #EF34F5;
-                cursor: pointer;
-                transition: all 0.3s;
-                margin-top: 25px;
-            }
-
-            button.see-details:hover {
-                box-shadow: 0px 0px 10px white;
-                transform: scale(1.05);
-            }
-            
-            button.login-already-user {
-                outline: none;
-                height: 40px;
-                background: transparent;
-                border: none;
-                color: white;
-                font-size: 1em;
-                cursor: pointer;
-                margin-top: 25px;
-                font-family: 'Oswald', sans-serif;
-                font-weight: 200;
-                background-color: rgba(0,0,0,0.2);
-                color: white;
-                transition: all 0.3s;
-            }
-
-            button.login-already-user:hover {
-                color: black;
-                background-color: white;
-            }
-
-            header h1,
-            header h2 {
-                font-family: 'Oswald', sans-serif;
-                color: white;
-            }
-            header h1 {
-                font-size: 6em;
-                font-weight: 500;
-            }
-            header h2 {
-                font-size: 2em;
-                font-weight: 300;
-            }
-            header h2 span.bold {
-                font-weight: 600;
-            }
-            header img {
-                width: 350px;
-                transform: rotate(2deg) scale(1.3) translateY(30px);
-            }
-            .lol {
-                background-color: red;
-            }
-            
-            main {
-            }
-
-            .main-features-cards {
-                display: flex;
-                flex-direction: row;
-                justify-content: center;
-                flex-wrap: wrap;
-            }
-
-            .feature-detail {
-                width: 100%;
-                height: 300px;
-                padding: 25px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-            .feature-detail:not(:last-child) {
-            }
-
-            .first-detail {
-            }
-
-            .second-detail {
-            }
-
+                .checkmark-place {
+                    width: 50px;
+                    height: 50px;
+                    background-color: ${checkmarkplaceColor};
+                    border-radius: 45%;
+                    position: relative;
+                }
+                img {
+                    width: 60px;
+                }
             `}
             </style>
         </>
     )
-
 }
