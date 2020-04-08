2c040d6d1dc4c8ff00958ba5de563d36554bbdd0
Adam Kiss <kiss.adam.a@gmail.com>
2020-04-08 20:14:12 +0300

    Add favicon

--- components/layoutSetup.js
+++ components/layoutSetup.js
@@ -9,6 +9,7 @@ export default ({ title = null }) => {
                     :
                     null
                 }
+                <link rel="icon" href="/static/favicon.png" />
                 <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=Reenie+Beanie&display=swap" rel="stylesheet"></link>
             </Head>
